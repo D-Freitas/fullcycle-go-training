6812fe35d4c0cbfe62fabc22bbf3dc99a2fa9bcc
D-Freitas <davi.israel01@gmail.com>
2022-11-08 16:52:54 -0300

    feat: ensure that when creating a new registration with no ID, receive an error

---
+++ internal/register-user/entity/register.go
@@ -0,0 +1,40 @@
+package entity
+
+import "errors"
+
+type Registration struct {
+	ID                   string
+	User                 string
+	FullName             string
+	Email                string
+	PhoneNumber          string
+	Password             string
+	PasswordConfirmation string
+}
+
+func NewRegistration(
+	id string,
+	user string,
+	fullname string,
+	email string,
+	phoneNumber string,
+	password string,
+	passwordConfirmation string,
+) (*Registration, error) {
+	return &Registration{
+		ID:                   id,
+		User:                 user,
+		FullName:             fullname,
+		Email:                email,
+		PhoneNumber:          phoneNumber,
+		Password:             password,
+		PasswordConfirmation: passwordConfirmation,
+	}, nil
+}
+
+func (r *Registration) IsValid() error {
+	if r.ID == "" {
+		return errors.New("invalid id")
+	}
+	return nil
+}
