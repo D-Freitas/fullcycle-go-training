df1ecc54765b574506f3c36a2ca8f876ae48f163
D-Freitas <davi.israel01@gmail.com>
2022-11-08 17:14:16 -0300

    test: ensure that when creating a new registration with no fullname, receive an error

--- internal/register-user/entity/register_test.go
+++ internal/register-user/entity/register_test.go
@@ -31,3 +31,16 @@ func TestGivenAnEmptyUser_WhenCreateNewRegistration_ThenShouldReceiveAnError(t *
 	}
 	assert.Error(t, registration.IsValid(), "invalid user")
 }
+
+func TestGivenAnEmptyFullname_WhenCreateNewRegistration_ThenShouldReceiveAnError(t *testing.T) {
+	registration := Registration{
+		"id_test",
+		"user_test",
+		"",
+		"email_test",
+		"phonenumber_test",
+		"password_test",
+		"password_confirmation_test",
+	}
+	assert.Error(t, registration.IsValid(), "invalid fullname")
+}
