b6d7d75a17dfdcf88c4b97a9caa70a41539a7523
D-Freitas <davi.israel01@gmail.com>
2022-11-08 16:58:25 -0300

    test: ensure that when creating a new registration with no user, receive an error

--- internal/register-user/entity/register_test.go
+++ internal/register-user/entity/register_test.go
@@ -18,3 +18,16 @@ func TestGivenAnEmptyID_WhenCreateNewRegistration_ThenShouldReceiveAnError(t *te
 	}
 	assert.Error(t, registration.IsValid(), "invalid id")
 }
+
+func TestGivenAnEmptyUser_WhenCreateNewRegistration_ThenShouldReceiveAnError(t *testing.T) {
+	registration := Registration{
+		"id_test",
+		"",
+		"fullname_test",
+		"email_test",
+		"phonenumber_test",
+		"password_test",
+		"password_confirmation_test",
+	}
+	assert.Error(t, registration.IsValid(), "invalid user")
+}
