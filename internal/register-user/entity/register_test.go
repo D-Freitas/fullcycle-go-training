d1153a8f26c559229ce03d8eb059679839c36a00
D-Freitas <davi.israel01@gmail.com>
2022-11-08 16:53:07 -0300

    test: ensure that when creating a new registration with no ID, receive an error

---
+++ internal/register-user/entity/register_test.go
@@ -0,0 +1,20 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGivenAnEmptyID_WhenCreateNewRegistration_ThenShouldReceiveAnError(t *testing.T) {
+	registration := Registration{
+		"",
+		"user_test",
+		"fullname_test",
+		"email_test",
+		"phonenumber_test",
+		"password_test",
+		"password_confirmation_test",
+	}
+	assert.Error(t, registration.IsValid(), "invalid id")
+}
