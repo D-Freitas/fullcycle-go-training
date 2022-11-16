b994adf10e3e7ef001fbfde3903f3b27be527822
D-Freitas <davi.israel01@gmail.com>
2022-11-16 13:18:48 -0300

    feat: ensure that given a password without uppercase letters, match return must be false

--- internal/register-user/entity/pattern-validator/password_validator.go
+++ internal/register-user/entity/pattern-validator/password_validator.go
@@ -6,13 +6,17 @@ import (
 
 func PasswordValidator(password string) bool {
 	letters := 0
+	isUpper := false
 	for _, c := range password {
 		switch {
+		case unicode.IsUpper(c):
+			isUpper = true
+			letters++
 		case unicode.IsLetter(c) || c == ' ':
 			letters++
 		}
 	}
 	containsSevenLettersOrMore := letters >= 7
-	isValid := containsSevenLettersOrMore
+	isValid := containsSevenLettersOrMore && isUpper
 	return isValid
 }
