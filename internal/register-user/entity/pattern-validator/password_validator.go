bddd5d34908ae62ddb47427dc181b07ec155960c
D-Freitas <davi.israel01@gmail.com>
2022-11-16 13:31:52 -0300

    feat: ensure that given a password without special chars, match return must be false

--- internal/register-user/entity/pattern-validator/password_validator.go
+++ internal/register-user/entity/pattern-validator/password_validator.go
@@ -6,17 +6,19 @@ import (
 
 func PasswordValidator(password string) bool {
 	letters := 0
-	isUpper := false
+	isUpper, isSymbol := false, false
 	for _, c := range password {
 		switch {
 		case unicode.IsUpper(c):
 			isUpper = true
 			letters++
+		case unicode.IsSymbol(c):
+			isSymbol = true
 		case unicode.IsLetter(c) || c == ' ':
 			letters++
 		}
 	}
 	containsSevenLettersOrMore := letters >= 7
-	isValid := containsSevenLettersOrMore && isUpper
+	isValid := containsSevenLettersOrMore && isUpper && isSymbol
 	return isValid
 }
