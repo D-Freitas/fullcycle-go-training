c825317c558697c2b66867e01448028ef1de920d
D-Freitas <davi.israel01@gmail.com>
2022-11-16 14:01:16 -0300

    feat: ensure that given a password less than ten, match return must be false

---
+++ internal/register-user/entity/pattern-validator/phone_number_validator.go
@@ -0,0 +1,9 @@
+package patternvalidator
+
+import "regexp"
+
+func PhoneNumberValidator(phoneNumber string) bool {
+	phoneNumberRegexp := regexp.MustCompile(`^\d{10,11}$`)
+	isMatch := phoneNumberRegexp.MatchString(phoneNumber)
+	return isMatch
+}
