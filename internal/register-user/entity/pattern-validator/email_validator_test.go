d3dc55fbe5bd8edaec9afbdbc949de5579aababa
D-Freitas <davi.israel01@gmail.com>
2022-11-16 12:33:47 -0300

    test: ensure that given an valid email, match return must be true

--- internal/register-user/entity/pattern-validator/email_validator_test.go
+++ internal/register-user/entity/pattern-validator/email_validator_test.go
@@ -10,3 +10,8 @@ func TestGivenAnInvalidEmail_WhenToCheck_ThenMatchShouldReturnFalse(t *testing.T
 	isMatch := EmailValidator("invalid_email")
 	assert.Equal(t, false, isMatch)
 }
+
+func TestGivenAnValidEmail_WhenToCheck_ThenMatchShouldReturnTrue(t *testing.T) {
+	isMatch := EmailValidator("davi.israel01@gmail.com")
+	assert.Equal(t, true, isMatch)
+}
